456866c953cf901acd6c795ae2086b87bc65916c
Peter Bourget <pbourget@w6op.com>
2022-05-08 13:27:55 -0700

    Initial Commit

---
+++ xLogExplorer/xLogExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  xLogExplorerApp.swift
+//  xLogExplorer
+//
+//  Created by Peter Bourget on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct xLogExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
